78c793f217e1999bdd6c40a7b91e203c1b6bb361
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2025-05-14 15:27:22 -0300

    feat: criar página administrativa com cabeçalho e rodapé

---
+++ Frontend/GPA/src/pages/AdminPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <Header />
+      <h1>Página Administrativa</h1>
+      <p>Aqui o administrador pode gerenciar alunos e módulos.</p>
+      <Footer />
+    </div>
+  );
+}

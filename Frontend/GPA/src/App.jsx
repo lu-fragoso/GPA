11e8129f14137e7830b508ce2133e5bf71fd06b8
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2025-05-18 23:32:19 -0300

    feat: adicionar rotas para páginas de adicionar aluno e matéria, e detalhes de aluno e matéria

--- Frontend/GPA/src/App.jsx
+++ Frontend/GPA/src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AlunoPage from "./pages/AlunoPage";
 import AdminPage from "./pages/AdminPage";
+import AddAlunoPage from "./pages/AddAlunoPage";
+import AddMateriaPage from "./pages/AddMateriaPage";
+import AlunoDetalhePage from "./pages/AlunoDetalhePage";
+import MateriaDetalhePage from "./pages/MateriaDetalhePage";
+
 
 function App() {
   return (
@@ -10,6 +15,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/aluno/:id" element={<AlunoPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin/add-aluno" element={<AddAlunoPage />} />
+        <Route path="/admin/add-materia" element={<AddMateriaPage />} />
+        <Route path="/admin/aluno/:id" element={<AlunoDetalhePage />} />
+        <Route path="/admin/materia/:id" element={<MateriaDetalhePage />} />
       </Routes>
     </Router>
   );
